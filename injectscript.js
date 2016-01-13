d328ec0543cf313db1e2c5a7285a7f4d65232579
Alexis Métaireau <alexis@notmyidea.org>
2016-01-13 16:28:15 +0100

    Inject the server url via a DOM node.

--- injectscript.js
+++ injectscript.js
@@ -1,3 +1,5 @@
+document.body.setAttribute("kinto-storage-url", "https://kinto.dev.mozaws.net/v1/")
+
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('script.js');
 s.onload = function() {
--- script.js
+++ script.js
@@ -2,6 +2,6 @@ if (window.storage == undefined)
   window.storage = {sync: {}};
 
 window.storage.sync.config = {
-  url: "https://kinto.dev.mozaws.net/v1/",
+  url: document.body.getAttribute("kinto-storage-url"),
   type: "kinto"
 };
